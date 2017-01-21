fe380056ee6203a050a5c21f2aaa6dcffbb59a56
ceyhuntasci <ceyhuntasci11@gmail.com>
2017-01-21 21:24:58 +0300

    Route Fixed

--- DNZ_Gayrimenkul/App_Start/RouteConfig.cs
+++ DNZ_Gayrimenkul/App_Start/RouteConfig.cs
@@ -16,7 +16,9 @@ namespace DNZ_Gayrimenkul
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "DNZ_Gayrimenkul.Controllers" }
+
             );
         }
     }
--- DNZ_Gayrimenkul/Global.asax.cs
+++ DNZ_Gayrimenkul/Global.asax.cs
@@ -16,6 +16,7 @@ namespace DNZ_Gayrimenkul
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
         }
     }
 }
