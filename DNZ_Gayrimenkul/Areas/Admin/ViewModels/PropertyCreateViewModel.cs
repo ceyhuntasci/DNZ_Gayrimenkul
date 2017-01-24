0d46f4bd4c110eff5ee9afa3c4eb1d9231cd915f
ceyhuntasci <ceyhuntasci11@gmail.com>
2017-01-24 21:10:25 +0300

    Property Create Edit Done

---
+++ DNZ_Gayrimenkul/Areas/Admin/ViewModels/PropertyCreateViewModel.cs
@@ -0,0 +1,17 @@
+﻿using DNZ_Gayrimenkul.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DNZ_Gayrimenkul.Areas.Admin.ViewModels
+{
+    public class PropertyCreateViewModel
+    {
+        public Property Property { get; set; }
+        public List<Specification> InnerSpecs { get; set; }
+        public List<Specification> OuterSpecs { get; set; }
+        public List<Specification> LocationSpecs { get; set; }
+        
+    }
+}
